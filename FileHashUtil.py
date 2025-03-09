51324eeac89bf489f9b81eeabf6a2123a4730af2
wquinones <wquinones@gmail.com>
2025-03-08 20:57:11 -0500

    Update FileHashUtil.py

--- FileHashUtil.py
+++ FileHashUtil.py
@@ -54,7 +54,14 @@ with tab1:
 
 with tab2:
     st.header("Verify File Hash")
-    user_hash = st.text_input("Enter the hash to compare against")
+    user_hash = st.text_input("Enter file hash for comparison")
+    
+    # Display supported hashes in smaller red font
+    st.markdown(
+        "<p style='color: red; font-size: 12px;'>Supported Hashes: MD5, SHA-1, SHA-256, SHA-384, SHA-512, SHA3-256, SHA3-384, SHA3-512</p>", 
+        unsafe_allow_html=True
+    )
+    
     file_to_verify = st.file_uploader("Upload file to verify", type=None)
     
     if file_to_verify and user_hash:
